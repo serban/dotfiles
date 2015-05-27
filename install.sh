2c04366f3b1740d68a3c6181968917b64cba888f
Serban Giuroiu <giuroiu@gmail.com>
2015-05-27 10:54:13 -0700

    install.sh: Add curly braces around $DOTFILES

--- install.sh
+++ install.sh
@@ -7,24 +7,24 @@ set -o pipefail
 
 readonly DOTFILES="${HOME}/src/dotfiles"
 
-ln -si $DOTFILES/bash_logout    ~/.bash_logout
-ln -si $DOTFILES/bashrc         ~/.bashrc
-ln -si $DOTFILES/colordiffrc    ~/.colordiffrc
-ln -si $DOTFILES/conkyrc        ~/.conkyrc
-ln -si $DOTFILES/dir_colors     ~/.dir_colors
-ln -si $DOTFILES/gdbinit        ~/.gdbinit
-ln -si $DOTFILES/gitconfig      ~/.gitconfig
-ln -si $DOTFILES/gitignore      ~/.gitignore
-ln -si $DOTFILES/gvimrc         ~/.gvimrc
-ln -si $DOTFILES/inputrc        ~/.inputrc
-ln -si $DOTFILES/screenrc       ~/.screenrc
-ln -si $DOTFILES/tmux.conf      ~/.tmux.conf
-ln -si $DOTFILES/vimrc          ~/.vimrc
-ln -si $DOTFILES/Xresources     ~/.Xresources
+ln -si ${DOTFILES}/bash_logout    ~/.bash_logout
+ln -si ${DOTFILES}/bashrc         ~/.bashrc
+ln -si ${DOTFILES}/colordiffrc    ~/.colordiffrc
+ln -si ${DOTFILES}/conkyrc        ~/.conkyrc
+ln -si ${DOTFILES}/dir_colors     ~/.dir_colors
+ln -si ${DOTFILES}/gdbinit        ~/.gdbinit
+ln -si ${DOTFILES}/gitconfig      ~/.gitconfig
+ln -si ${DOTFILES}/gitignore      ~/.gitignore
+ln -si ${DOTFILES}/gvimrc         ~/.gvimrc
+ln -si ${DOTFILES}/inputrc        ~/.inputrc
+ln -si ${DOTFILES}/screenrc       ~/.screenrc
+ln -si ${DOTFILES}/tmux.conf      ~/.tmux.conf
+ln -si ${DOTFILES}/vimrc          ~/.vimrc
+ln -si ${DOTFILES}/Xresources     ~/.Xresources
 
 mkdir -p ~/.moc
-ln -si $DOTFILES/moc/config     ~/.moc/config
-ln -si $DOTFILES/moc/themes     ~/.moc/themes
+ln -si ${DOTFILES}/moc/config     ~/.moc/config
+ln -si ${DOTFILES}/moc/themes     ~/.moc/themes
 
 # Silence the message of the day and last login info
 touch ~/.hushlogin
@@ -34,7 +34,7 @@ OS="$(uname -s)"
 if [[ "$(whoami)" = 'root' ]] || \
    [[ "${OS}" = 'Darwin' ]] || \
    [[ "${OS}" = 'FreeBSD' ]]; then
-  ln -si $DOTFILES/bashrc ~/.bash_profile
+  ln -si ${DOTFILES}/bashrc ~/.bash_profile
 fi
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
