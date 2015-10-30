serban/dotfiles

commit 2024d13980b62c9822ec15a2f9ba17a67e7810e4

install-homebrew-packages.sh: Swap vim for vim-nox

setup/install-homebrew-packages.sh
@@ -19,4 +19,4 @@ brew install \
   python3 \
   rsync \
   tmux \
-  vim
+  vim-nox

commit 7e30536124d4d5d98f3da3ea715593c5ddd8034d

install-debian-packages.sh: Install vim-nox
Undo commit 2024d13980b62c9822ec15a2f9ba17a67e7810e4, which mistakenly made this
change in install-homebrew-packages.sh instead of install-debian-packages.sh.

setup/install-debian-packages.sh
@@ -16,7 +16,7 @@ sudo apt-get install \
   python3 \
   tmux \
   unzip \
-  vim
+  vim-nox
 
 # Optionally add Debian testing to /etc/apt/sources.list and then run:
-# sudo apt-get install --only-upgrade git tmux vim
+# sudo apt-get install --only-upgrade git tmux vim-nox

setup/install-homebrew-packages.sh
@@ -19,4 +19,4 @@ brew install \
   python3 \
   rsync \
   tmux \
-  vim-nox
+  vim