ec45e335b94766804d2b14abecea9644549ee12c
Serban Giuroiu <giuroiu@gmail.com>
2018-12-31 02:37:27 -0800

    fish: serban_time_marker_with_duration: Add space

--- fish/functions/serban_time_marker_with_duration.fish
+++ fish/functions/serban_time_marker_with_duration.fish
@@ -1,5 +1,5 @@
 function serban_time_marker_with_duration --argument-names duration_sec
-  printf '%s %s(%s)%s' \
+  printf '%s  %s(%s)%s' \
       (serban_time_marker) \
       (set_color yellow) \
       (serban_format_duration $duration_sec) \
