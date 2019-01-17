86681ce8785c23c8ecf1f1a2a1fa0b39b582f8d6
Serban Giuroiu <giuroiu@gmail.com>
2019-01-17 14:15:20 -0800

    fish:  Add helper for printing visible headers

---
+++ fish/functions/serban_header.fish
@@ -0,0 +1,12 @@
+function serban_header
+  set --local pad 76
+  set --local len (string length "$argv")
+
+  if test $len -gt $pad
+    set pad $len
+  end
+
+  python3 -c "print('+-{}-+'.format('-'*$pad));    \
+              print('| {:$pad} |'.format('$argv')); \
+              print('+-{}-+'.format('-'*$pad));    "
+end
