5058c47b15154545bc577c762a1771940852829a
Serban Giuroiu <giuroiu@gmail.com>
2019-02-01 13:44:20 -0800

    fish: mercurial: Add rebase and resolve

--- fish/conf.d/mercurial.fish
+++ fish/conf.d/mercurial.fish
@@ -1,5 +1,7 @@
 abbr --add --global had hg add
 abbr --add --global hco hg commit
 abbr --add --global hdi hg diff
+abbr --add --global hrb hg rebase
+abbr --add --global hrs hg resolve
 abbr --add --global hst hg status
 abbr --add --global hup hg update
