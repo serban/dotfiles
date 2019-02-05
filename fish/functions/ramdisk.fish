4981fe9cb1025553d298f8f77afcb29a027f6df4
Serban Giuroiu <giuroiu@gmail.com>
2019-02-05 12:59:56 -0800

    fish: Add a function to mount ramdisks

---
+++ fish/functions/ramdisk.fish
@@ -0,0 +1,19 @@
+function ramdisk --argument-names size_gibibytes
+  if test -z "$size_gibibytes"
+    echo 'You must specify a size in gibibytes'
+    return 1
+  end
+
+  if type --quiet diskutil
+    echo "Mounting ramdisk at /Volumes/ramdisk with size $size_gibibytes GiB"
+    set --local block_size (math --scale=0 "2048 * 1024 * $size_gibibytes")
+    set --local disk (string trim (hdiutil attach -nomount ram://$block_size))
+    diskutil eraseVolume HFS+ ramdisk $disk
+    return
+  end
+
+  echo "Mounting ramdisk at /ramdisk with size $size_gibibytes GiB"
+  set --local size_bytes (math --scale=0 "1024 ^ 3 * $size_gibibytes")
+  sudo mkdir -p /ramdisk
+  and sudo mount -t tmpfs -o size=$size_bytes tmpfs /ramdisk
+end
