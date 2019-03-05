fc4d289c593ce90daf41cc813848bb9e5530fe40
Serban Giuroiu <giuroiu@gmail.com>
2019-03-05 12:12:21 -0800

    fish: mercurial: Add abbreviations for hg log

--- fish/conf.d/mercurial.fish
+++ fish/conf.d/mercurial.fish
@@ -1,6 +1,8 @@
 abbr --add --global had hg add
 abbr --add --global hco hg commit
 abbr --add --global hdi hg diff
+abbr --add --global hlo hg log --stat
+abbr --add --global hlp hg log --patch --stat
 abbr --add --global hrb hg rebase
 abbr --add --global hrs hg resolve
 abbr --add --global hst hg status
