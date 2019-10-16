21b5e2cf4fd23d2c47918076deeb0995ae7771f5
Serban Giuroiu <giuroiu@gmail.com>
2019-10-16 13:48:28 -0700

    fish: mercurial: Add fig abbreviations

--- fish/conf.d/mercurial.fish
+++ fish/conf.d/mercurial.fish
@@ -1,9 +1,18 @@
 abbr --add --global had hg add
+abbr --add --global ham hg amend
 abbr --add --global hco hg commit
 abbr --add --global hdi hg diff
+abbr --add --global hev hg evolve
 abbr --add --global hlo hg log --stat
 abbr --add --global hlp hg log --patch --stat
+abbr --add --global hme hg meld
 abbr --add --global hrb hg rebase
 abbr --add --global hrs hg resolve
 abbr --add --global hst hg status
 abbr --add --global hup hg update
+
+# Fig Aliases
+abbr --add --global hau 'hg amend && hg uploadchain && hg xl'
+abbr --add --global hae 'hg amend && hg evolve && hg uploadchain && hg xl'
+abbr --add --global huc 'hg uploadchain && hg xl'
+abbr --add --global hxl 'hg xl'
