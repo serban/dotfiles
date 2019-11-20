2aa9b181dcf86f267c2e43c3e8ab2a1fc897506f
Serban Giuroiu <giuroiu@gmail.com>
2019-11-20 13:08:28 -0800

    launch: Update launchers with some lighter tools

--- setup/install-debian-packages.sh
+++ setup/install-debian-packages.sh
@@ -5,6 +5,7 @@ set -o nounset
 set -o pipefail
 
 sudo apt-get install \
+  bpython3 \
   bzip2 \
   clang \
   colordiff \
@@ -33,16 +34,22 @@ sudo apt-get install \
 # sudo apt-get install --only-upgrade git tmux vim-nox
 
 sudo apt-get install \
+  alsa-utils \
   conky \
   feh \
   fluxbox \
   graphviz \
   i3 \
+  nitrogen \
   numlockx \
+  rofi \
   rxvt-unicode \
+  thunar \
   vim-gtk \
   virtualbox-guest-dkms \
   virtualbox-guest-utils \
   virtualbox-guest-x11 \
   xautolock \
-  xdotool
+  xdotool \
+  xfce4-screenshooter \
+  xinput
