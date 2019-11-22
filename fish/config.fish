108e6685213c8aee8dc08a685dfc8a181f9085fe
Serban Giuroiu <giuroiu@gmail.com>
2019-11-22 14:16:52 -0800

    fish: Add abbrevations for find, jobs, and kill

--- fish/config.fish
+++ fish/config.fish
@@ -95,8 +95,12 @@ abbr --add --global t tail
 abbr --add --global l less -N
 
 abbr --add --global d colordiff -u
+abbr --add --global f find . -iname
 
 abbr --add --global i history
+
+abbr --add --global j jobs
+abbr --add --global k kill
 
 abbr --add --global cp cp -vi
 abbr --add --global mv mv -vi
@@ -106,6 +110,9 @@ abbr --add --global p2 python2
 abbr --add --global p3 python3
 
 abbr --add --global kb xset r rate 200 50  # in milliseconds and keystrokes/sec
+
+abbr --add --global rmds find . -type f -name .DS_Store -exec rm -vf "'{}'" \\\;
+abbr --add --global rmswp find . -type f -regex "'^.*/\\.[^/]*sw[a-z]\$'" -exec rm -vf "'{}'" \\\;
 
 abbr --add --global wmt watchman-make --pattern "'**/BUILD'" "'**/*.h'" "'**/*.cc'" --run "'bazel test :all'"
 
