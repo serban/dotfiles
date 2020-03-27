70be4f61061e2929864a80c0257e932a84975ff2
Serban Giuroiu <giuroiu@gmail.com>
2020-03-27 09:21:26 -0700

    fish: piper: pre: Automate code reviews patching

--- fish/conf.d/piper.fish
+++ fish/conf.d/piper.fish
@@ -19,6 +19,21 @@ function pdi --argument-names file
   env P4DIFF='colordiff -u' g4 diff $file | less
 end
 
+function pre --argument-names changelist
+  if test -z $changelist
+    echo 'No changelist specified'
+    return 1
+  end
+
+  set --local client review-$changelist
+
+  g4d -f $client
+  g4 patch -c $changelist
+  g4 change --desc "Review of cl/$changelist"
+
+  mag $client
+end
+
 abbr --add --global pbu 'g4 change --desc "g3doc bump @ "(date "+%Y-%m-%d %H:%M")'
 abbr --add --global pdc 'g4 citc -d -f'
 abbr --add --global pew 'e (g4 whatsout)'
