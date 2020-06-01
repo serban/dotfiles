91ec8240992bcf07c392c5518a31494c818367b6
Serban Giuroiu <giuroiu@gmail.com>
2020-06-01 11:18:18 -0700

    install.sh: Factor out directories into variables

--- install.sh
+++ install.sh
@@ -5,6 +5,35 @@ set -o nounset
 set -o pipefail
 
 readonly DOTFILES="${HOME}/src/dotfiles"
+readonly OS="$(uname -s)"
+readonly WHOAMI="$(whoami)"
+
+function darwin {
+  [ "${OS}" = 'Darwin' ]
+}
+
+function freebsd {
+  [ "${OS}" = 'FreeBSD' ]
+}
+
+function linux {
+  [ "${OS}" = 'Linux' ]
+}
+
+function root {
+  [ "${WHOAMI}" = 'root' ]
+}
+
+darwin && {
+  readonly        ITERM="${HOME}/Library/Application Support/iTerm2/Scripts"
+  readonly SUBLIME_TEXT="${HOME}/Library/Application Support/Sublime Text 3/Packages/User"
+  readonly       VSCODE="${HOME}/Library/Application Support/Code/User"
+}
+
+linux && {
+  readonly SUBLIME_TEXT="${HOME}/.config/sublime-text-3/Packages/User"
+  readonly       VSCODE="${HOME}/.config/Code/User"
+}
 
 ln -si ${DOTFILES}/bash_logout        ~/.bash_logout
 ln -si ${DOTFILES}/bashrc             ~/.bashrc
@@ -53,47 +82,38 @@ mkdir -p -m 700 ~/.history
 # Silence the message of the day and last login info
 touch ~/.hushlogin
 
-OS="$(uname -s)"
-
-if [[ "$(whoami)" = 'root' ]] || \
-   [[ "${OS}" = 'Darwin' ]] || \
-   [[ "${OS}" = 'FreeBSD' ]]; then
+root || darwin || freebsd && {
   ln -si ${DOTFILES}/bashrc ~/.bash_profile
-fi
+}
 
-if [[ "${OS}" = 'Darwin' ]]; then
+darwin && {
   mkdir -p ~/bin
   ln -si  /Applications/LilyPond.app/Contents/Resources/bin/lilypond ~/bin/
   ln -si '/Applications/Sublime Merge.app/Contents/SharedSupport/bin/smerge' ~/bin/
   ln -si '/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl' ~/bin/
-fi
+}
 
-if [[ "${OS}" = 'Darwin' ]]; then
-  mkdir -p ~/Library/'Application Support'/iTerm2/Scripts/AutoLaunch
-  ln -si ${DOTFILES}/iterm2/profile.py ~/Library/'Application Support'/iTerm2/Scripts/AutoLaunch/
-  ln -si ${DOTFILES}/iterm2/font_12.py ~/Library/'Application Support'/iTerm2/Scripts/
-  ln -si ${DOTFILES}/iterm2/font_15.py ~/Library/'Application Support'/iTerm2/Scripts/
-  ln -si ${DOTFILES}/iterm2/font_18.py ~/Library/'Application Support'/iTerm2/Scripts/
-  ln -si ${DOTFILES}/iterm2/font_24.py ~/Library/'Application Support'/iTerm2/Scripts/
-  ln -si ${DOTFILES}/iterm2/solarized_dark.py ~/Library/'Application Support'/iTerm2/Scripts/
-  ln -si ${DOTFILES}/iterm2/solarized_light.py ~/Library/'Application Support'/iTerm2/Scripts/
-fi
+darwin && {
+  mkdir -p "${ITERM}/AutoLaunch"
+  ln -si ${DOTFILES}/iterm2/profile.py          "${ITERM}/AutoLaunch/"
+  ln -si ${DOTFILES}/iterm2/font_12.py          "${ITERM}/"
+  ln -si ${DOTFILES}/iterm2/font_15.py          "${ITERM}/"
+  ln -si ${DOTFILES}/iterm2/font_18.py          "${ITERM}/"
+  ln -si ${DOTFILES}/iterm2/font_24.py          "${ITERM}/"
+  ln -si ${DOTFILES}/iterm2/solarized_dark.py   "${ITERM}/"
+  ln -si ${DOTFILES}/iterm2/solarized_light.py  "${ITERM}/"
+}
 
-if [[ "${OS}" = 'Darwin' ]]; then
-  mkdir -p "${HOME}/Library/Application Support/Sublime Text 3/Packages/User"
-  ln -si ${DOTFILES}/sublime/Preferences.sublime-settings "${HOME}/Library/Application Support/Sublime Text 3/Packages/User/"
-  ln -si "${DOTFILES}/sublime/Package Control.sublime-settings" "${HOME}/Library/Application Support/Sublime Text 3/Packages/User/"
-else
-  mkdir -p ~/.config/sublime-text-3/Packages/User
-  ln -si ${DOTFILES}/sublime/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/
-  ln -si "${DOTFILES}/sublime/Package Control.sublime-settings" ~/.config/sublime-text-3/Packages/User/
-fi
+darwin || linux && {
+  mkdir -p "${SUBLIME_TEXT}"
+  ln -si  ${DOTFILES}/sublime/Preferences.sublime-settings      "${SUBLIME_TEXT}/"
+  ln -si "${DOTFILES}/sublime/Package Control.sublime-settings" "${SUBLIME_TEXT}/"
+}
 
-if [[ "${OS}" = 'Darwin' ]]; then
-  readonly VSCODE="${HOME}/Library/Application Support/Code/User"
+darwin || linux && {
   mkdir -p "${VSCODE}"
   ln -si ${DOTFILES}/vscode/settings.json "${VSCODE}/"
-fi
+}
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +quitall
