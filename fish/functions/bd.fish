02656a6ea5e2280ffe8704ecc58e3aa52af64803
Serban Giuroiu <giuroiu@gmail.com>
2020-12-23 13:25:54 -0800

    fish: bd: Diff installed packages against expected

---
+++ fish/functions/bd.fish
@@ -0,0 +1,52 @@
+function bd
+  set --local options (fish_opt --short=m --long=missing)
+
+  if not argparse $options -- $argv
+    return
+  end
+
+  set --local packages (cat ~/src/dotfiles/setup/homebrew-*.txt)
+  set --local output_dir (gmktemp --directory --tmpdir 'bd.XXXXXX')
+
+  set --local expected          '1-expected.txt'
+  set --local expected_sorted   '2-expected-sorted.txt'
+  set --local leaves_sorted     '3-leaves-sorted.txt'
+  set --local installed_sorted  '4-installed-sorted.txt'
+  set --local removable         '5-removable.txt'
+  set --local leaves_added      '6-leaves-added.txt'
+  set --local packages_added    '7-packages-added.txt'
+  set --local packages_missing  '8-packages-missing.txt'
+
+  pushd $output_dir
+
+  for package in $packages
+    echo $package >> $expected
+  end
+  brew deps --union --full-name $packages >> $expected
+  sort --unique $expected > $expected_sorted
+
+  brew leaves | sort --unique > $leaves_sorted
+  brew list --formula --full-name | sort --unique > $installed_sorted
+  brew autoremove --dry-run > $removable
+
+  comm -13 $expected_sorted $leaves_sorted > $leaves_added
+  comm -13 $expected_sorted $installed_sorted > $packages_added
+  comm -23 $expected_sorted $installed_sorted > $packages_missing
+
+  header 'Leaves Added'
+  cat $leaves_added
+
+  header 'Packages Added'
+  cat $packages_added
+
+  if test -n "$_flag_missing"
+    header 'Packages Missing'
+    cat $packages_missing
+  end
+
+  header 'Packages Removable'
+  cat $removable
+
+  popd
+  rm -r $output_dir
+end
--- setup/install-homebrew-packages.sh
+++ setup/install-homebrew-packages.sh
@@ -4,35 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-brew install \
-  bazel \
-  colordiff \
-  coreutils \
-  duti \
-  editorconfig \
-  findutils \
-  fish \
-  gawk \
-  git \
-  gnu-sed \
-  gnu-tar \
-  go \
-  graphviz \
-  grep \
-  grip \
-  hashdeep \
-  hexyl \
-  httpie \
-  hugo \
-  jq \
-  less \
-  macvim \
-  pastel \
-  pipenv \
-  python \
-  rsync \
-  sqlite \
-  the_silver_searcher \
-  tmux \
-  watch \
-  watchman
+brew tap \
+  heroku/brew
+
+cat homebrew-core.txt | xargs brew install
