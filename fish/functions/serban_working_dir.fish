78cf525b8114cda3bc9344e6699b8e086cc00424
Serban Giuroiu <giuroiu@gmail.com>
2021-02-23 12:41:20 -0800

    fish: serban_working_dir: Strip home dir prefix

--- fish/functions/serban_working_dir.fish
+++ fish/functions/serban_working_dir.fish
@@ -1,6 +1,11 @@
+# See https://github.com/fish-shell/fish-shell/blob/master/share/functions/prompt_pwd.fish
+
 function serban_working_dir
+  set --local home ~
+  set --local pwd (string replace --regex '^'"$home"'/' '~/' $PWD)
+
   string replace --regex \
       '^/google/src/cloud/serban/[^/]+/google3/' \
       'google3/' \
-      (pwd)
+      $pwd
 end
