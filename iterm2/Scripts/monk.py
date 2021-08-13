0296758343a709464bf36e9a60b404966302ccfd
Serban Giuroiu <giuroiu@gmail.com>
2021-08-13 14:15:59 -0700

    iTerm2: monk.py: Open tabs in a new window

--- iterm2/Scripts/monk.py
+++ iterm2/Scripts/monk.py
@@ -11,15 +11,22 @@ async def main(connection):
 
   app = await iterm2.async_get_app(connection)
 
+  window = await iterm2.Window.async_create(connection)
+  default_tab = window.current_tab
+
   sessions = []
   for c in commands:
-    tab = await app.current_window.async_create_tab('monk')
+    tab = await window.async_create_tab('monk')
     sessions.append(tab.current_session)
+
+  await default_tab.async_close()
 
   time.sleep(1)
 
   for i, c in enumerate(commands):
     await sessions[i].async_send_text(c + '\n')
+
+  await window.tabs[0].async_activate()
 
 if __name__ == '__main__':
   iterm2.run_until_complete(main)
