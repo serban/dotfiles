5ae3524af5ca3718a87674b8137aaf8fee5ed52e
Serban Giuroiu <giuroiu@gmail.com>
2021-09-09 10:58:56 -0700

    toolkit-for-ynab.json: Settings for 2.35.1
    Export settings for Toolkit for YNAB 2.35.1.

--- extensions/format.sh
+++ extensions/format.sh
@@ -4,5 +4,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-cat autopintab.json  | jq             | sponge autopintab.json
-cat dark-reader.json | jq --sort-keys | sponge dark-reader.json
+cat autopintab.json       | jq             | sponge autopintab.json
+cat dark-reader.json      | jq --sort-keys | sponge dark-reader.json
+cat toolkit-for-ynab.json | jq --sort-keys | sponge toolkit-for-ynab.json
