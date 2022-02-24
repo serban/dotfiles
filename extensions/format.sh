b4034b6709acac3687abf6af33d4fff2015c32e5
Serban Giuroiu <giuroiu@gmail.com>
2022-02-24 09:54:37 -0800

    sidebery.json: Settings for 4.10.0
    Export settings for Sidebery 4.10.0.

--- extensions/format.sh
+++ extensions/format.sh
@@ -6,4 +6,5 @@ set -o pipefail
 
 cat autopintab.json       | jq             | sponge autopintab.json
 cat dark-reader.json      | jq --sort-keys | sponge dark-reader.json
+cat sidebery.json         | jq --sort-keys | sponge sidebery.json
 cat toolkit-for-ynab.json | jq --sort-keys | sponge toolkit-for-ynab.json
