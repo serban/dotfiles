6cae6cc0fdad7a91208eb9ec8aaf17d6234b5c3f
Serban Giuroiu <giuroiu@gmail.com>
2022-04-03 13:58:26 -0700

    init.lua: Maximize and center new Preview windows

--- init.lua
+++ init.lua
@@ -192,7 +192,7 @@ logScreens()
 
 local wf = hs.window.filter.new(false, 'serban-wf', 'warning')
 for _, app in ipairs({'Firefox', 'Google Chrome', 'MacVim', 'OmniFocus',
-                      'Sublime Merge', 'iTerm2'}) do
+                      'Preview', 'Sublime Merge', 'iTerm2'}) do
   wf:setAppFilter(app, {allowRoles='AXStandardWindow'})
 end
 wf:subscribe(hs.window.filter.windowCreated, function(w, app, event)
