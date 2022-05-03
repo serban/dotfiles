d0da60524902b948b0ef2f5f7b8957a90a9684ad
Serban Giuroiu <giuroiu@gmail.com>
2022-05-03 10:11:12 -0700

    iTerm2: local.py: Open a tab for each command

---
+++ iterm2/Scripts/local.py
@@ -0,0 +1,32 @@
+import pathlib
+import time
+
+import iterm2
+
+COMMANDS_PATH = pathlib.Path(pathlib.Path.home(), 'tmp', 'local.sh')
+
+async def main(connection):
+  with open(COMMANDS_PATH) as f:
+    commands = [l for l in f.read().splitlines() if l and not l.startswith('#')]
+
+  app = await iterm2.async_get_app(connection)
+
+  window = await iterm2.Window.async_create(connection)
+  default_tab = window.current_tab
+
+  sessions = []
+  for c in commands:
+    tab = await window.async_create_tab()
+    sessions.append(tab.current_session)
+
+  await default_tab.async_close()
+
+  time.sleep(1)
+
+  for i, c in enumerate(commands):
+    await sessions[i].async_send_text(c + '\n')
+
+  await window.tabs[0].async_activate()
+
+if __name__ == '__main__':
+  iterm2.run_until_complete(main)
