13df2d1031b027b737b104bc5b9433a772e8a868
Serban Giuroiu <giuroiu@gmail.com>
2023-02-21 15:36:17 -0800

    fish: fn: ctrl-r: enter: ⌃R ↦ Reload; ⌃M ↦ Open

--- fish/functions/fn.fish
+++ fish/functions/fn.fish
@@ -8,7 +8,10 @@ function fn
             --preview "bat --color always --theme '$theme' --plain ~/txt/{}" \
             --bind 'ctrl-g:change-preview-window(right,80|bottom,60%|hidden|)' \
             --bind 'ctrl-o:execute-silent(open -R ~/txt/{})' \
-            --print0 \
-      | gsed --null-data 's|^|~/txt/|' \
-      | xargs -0 mvim --remote-tab-silent
+            --bind 'ctrl-r:reload(fd --base-directory ~/txt --strip-cwd-prefix --type file)' \
+            --bind 'double-click:execute-silent(mvim --remote-tab-silent ~/txt/{})' \
+            --bind 'enter:execute-silent(mvim --remote-tab-silent ~/txt/{})'
+#           --print0 \
+#     | gsed --null-data 's|^|~/txt/|' \
+#     | xargs -0 mvim --remote-tab-silent
 end
