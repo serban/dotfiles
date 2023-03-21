1ea0e9eb9499b61a0e292b6ce7bca3ecf5766558
Serban Giuroiu <giuroiu@gmail.com>
2023-03-21 09:36:25 -0700

    fish: mercurial: hrt: Use box drawing characters

--- fish/conf.d/mercurial.fish
+++ fish/conf.d/mercurial.fish
@@ -70,10 +70,10 @@ function hrt
   set --function width_outer 110
   set --function width_inner (math $width_outer - 2)
 
-  printf '| ✓ | %-'$width_inner's | Notes |\n' File
-  printf '|---|%s|-------|\n' (string repeat --no-newline --count $width_outer -)
+  printf '│ ✓ │ %-'$width_inner's │ ‹ Notes │\n' '‹ File'
+  printf '├───┼%s┼─────────┤\n' (string repeat --no-newline --count $width_outer ─)
 
   for f in (hg status --no-status --change . | grep --invert-match 'BUILD$' | sort)
-    printf '| · | %-'$width_inner's |       |\n' $f
+    printf '│ · │ %-'$width_inner's │         │\n' $f
   end
 end
