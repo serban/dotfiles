68a9af2aadd2243bef293a1fecd8118aa2bfdbec
Serban Giuroiu <giuroiu@gmail.com>
2023-09-09 14:37:49 +0300

    fish: workdays: Exclude leave

--- fish/functions/workdays.fish
+++ fish/functions/workdays.fish
@@ -2,6 +2,7 @@ function workdays --wraps days
   days \
     --exclude ~/src/private/calendar/holidays.txt \
     --exclude ~/src/private/calendar/vacations.txt \
+    --exclude ~/src/private/calendar/leave.txt \
     --exclude ~/src/private/calendar/oncall.txt \
     --exclude-weekends \
     $argv
