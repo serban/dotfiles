1af8bbcaf8917c710d1f6bc14be314bbb303a305
Serban Giuroiu <giuroiu@gmail.com>
2023-10-04 15:55:30 +0300

    make-home-dirs.sh: Create common folders in $HOME

---
+++ setup/make-home-dirs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+mkdir -vp ~/bin ~/log ~/oss ~/pkg ~/pre ~/ses ~/src ~/txt ~/uvd
+
+if [[ "$(uname -s)" == Darwin ]]; then
+  mkdir -vp ~/Data ~/Deletable ~/Screenshots ~/Workspace
+fi
