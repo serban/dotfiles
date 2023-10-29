76c5c3d472239c7202c44c0e5cf3721fb4e6f052
Serban Giuroiu <giuroiu@gmail.com>
2023-10-29 12:27:36 -0700

    install.sh: Set XDG base directory variables
    • XDG_CONFIG_HOME
    • XDG_DATA_HOME
    • XDG_STATE_HOME
    • XDG_CACHE_HOME

--- fish/config.fish
+++ fish/config.fish
@@ -64,6 +64,11 @@ set --global fish_pager_color_description green
 
 set --global fish_greeting
 
+set --global --export XDG_CONFIG_HOME $HOME/.config
+set --global --export XDG_DATA_HOME   $HOME/.local/share
+set --global --export XDG_STATE_HOME  $HOME/.local/state
+set --global --export XDG_CACHE_HOME  $HOME/.cache
+
 set --global --export LD_LIBRARY_PATH \
     $HOME/homebrew/lib \
     $LD_LIBRARY_PATH
