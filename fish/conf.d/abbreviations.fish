63d415bb9bbc17127874717a71b4f9c7a600d8c0
Serban Giuroiu <giuroiu@gmail.com>
2023-10-30 09:12:23 -0700

    fish: 3: Leave space between the cursor and pipe

--- fish/conf.d/abbreviations.fish
+++ fish/conf.d/abbreviations.fish
@@ -4,7 +4,7 @@ abbr --add l less -N
 
 abbr --add d colordiff -u
 abbr --add f find . -iname
-abbr --add 3 --set-cursor=° 'tree -C °| less'
+abbr --add 3 --set-cursor=° 'tree -C ° | less'
 
 abbr --add i " history merge && history --show-time=\e'[33m[%Y-%m-%d] '\e'[0m' --reverse"
 
