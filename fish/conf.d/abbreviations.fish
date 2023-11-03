82ee7448bb059cc444f13321017f790c3856adcf
Serban Giuroiu <giuroiu@gmail.com>
2023-11-02 19:10:43 -0700

    fish: abbr: fdh: Don't exclude go/ anymore
    Excluding ~/go is not necessary after setting GOPATH to ~/env/go in
    commit 59df4faad89a0d75389167e5a656c25a31149558.

--- fish/conf.d/abbreviations.fish
+++ fish/conf.d/abbreviations.fish
@@ -60,7 +60,7 @@ abbr --add sts "date '+%Y-%m-%d-%H%M%S'"
 # * https://docs.rs/ignore/latest/ignore/struct.WalkBuilder.html
 #
 # Tested on 2021-12-22 with fd 8.3.0.
-abbr --add fdh 'fd -E /env -E /go -E /opt -E /oss -E /pre -E /Library -E /Movies -E /Music -E /Pictures -E /Data'
+abbr --add fdh 'fd -E /env -E /opt -E /oss -E /pre -E /Library -E /Movies -E /Music -E /Pictures -E /Data'
 
 abbr --add mocp 'mocp --moc-dir ~/.config/moc'
 
