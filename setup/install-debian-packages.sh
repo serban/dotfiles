b161268cb1caed2e0860c73ccf80742056617502
Serban Giuroiu <giuroiu@gmail.com>
2023-11-03 14:25:21 -0700

    packages: Refactor Python into libs, REPLs, tools

--- setup/install-debian-packages.sh
+++ setup/install-debian-packages.sh
@@ -4,4 +4,4 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-cat ~/src/dotfiles/packages/debian-core.txt | xargs sudo apt-get install --yes
+cat ~/src/dotfiles/packages/debian-{core,py-libs,py-repls,py-tools}.txt | xargs sudo apt-get install --yes
--- setup/install-pip-packages.sh
+++ setup/install-pip-packages.sh
@@ -7,9 +7,7 @@ set -o pipefail
 readonly PIP_REQUIRE_VIRTUALENV=false
 
 if [[ "$(uname -s)" == Darwin ]]; then
-  cat ~/src/dotfiles/packages/pip.txt | xargs pip3 install
+  cat ~/src/dotfiles/packages/pip-{py-libs,py-repls}.txt | xargs pip3 install
 else
-  cat ~/src/dotfiles/packages/pip.txt | xargs pip3 install --user
+  cat ~/src/dotfiles/packages/pip-{py-libs,py-repls}.txt | xargs pip3 install --user
 fi
-
-fish -c 'vf install auto_activation'
