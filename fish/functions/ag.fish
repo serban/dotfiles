34de34b5eb3adf9bbbacff63a5214f001430596c
Serban Giuroiu <giuroiu@gmail.com>
2023-11-29 14:24:11 -0800

    fish: ag: Add flag for multiple search directories

--- fish/functions/ag.fish
+++ fish/functions/ag.fish
@@ -1,5 +1,6 @@
 function ag --wraps ag
   set --local  options (fish_opt --short=c --long=cpp)
+  set options $options (fish_opt --short=d --long=directory --multiple-vals)
   set options $options (fish_opt --short=f --long=files)
   set options $options (fish_opt --short=g --long=go)
   set options $options (fish_opt --short=h --long=html)
@@ -25,6 +26,16 @@ function ag --wraps ag
 
   if test -n "$_flag_cpp"
     set args $args --cc --cpp
+  end
+
+  if test -n "$_flag_directory"
+    header Directories
+    set_color green
+    for dir in $_flag_directory
+      echo • $dir
+    end
+    set_color normal
+    header Results
   end
 
   if test -n "$_flag_files"
@@ -93,5 +104,5 @@ function ag --wraps ag
     printf '%s⁖ %s ⁙%s\n\n' (set_color magenta) $argv[1] (set_color normal)
   end
 
-  command ag $args $argv
+  command ag $args $argv $_flag_directory
 end
