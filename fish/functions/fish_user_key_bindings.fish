63c374fe9682af68859366a8e2b6c701b1bf6aeb
Serban Giuroiu <giuroiu@gmail.com>
2023-12-02 10:47:09 -0800

    fish: fish_user_key_bindings: Append head and tail
    This is in the spirit of the default binding `⌥ + P` which pipes the
    command into a pager:
    
    • https://fishshell.com/docs/current/interactive.html
    • https://github.com/fish-shell/fish-shell/blob/master/share/functions/fish_default_key_bindings.fish
    • https://github.com/fish-shell/fish-shell/blob/master/share/functions/__fish_shared_key_bindings.fish
    • https://github.com/fish-shell/fish-shell/blob/master/share/functions/__fish_paginate.fish
    
    │ Binding │ ‹ Append        │ Default │
    ├─────────┼─────────────────┼─────────┤
    │ `⌥ + P` │ `&| less`       │    ✓    │
    │ `⌥ + A` │ `&| head -n 20` │         │
    │ `⌥ + Z` │ `&| tail -n 20` │         │

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -5,4 +5,6 @@ function fish_user_key_bindings
   bind \e\x7f backward-kill-bigword
   bind \ec    print_working_dir
   bind \em    serban_fish_prompt_minimal_toggle
+  bind \ea    'fish_commandline_append " &| head -n 20"'
+  bind \ez    'fish_commandline_append " &| tail -n 20"'
 end
