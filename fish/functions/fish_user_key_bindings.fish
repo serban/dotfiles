d9cf9c09ce0e7771b937e19ef52f7759c77c9edd
Serban Giuroiu <giuroiu@gmail.com>
2024-02-24 13:31:08 -0800

    fish: fish_user_key_bindings: Insert middle dot

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -5,6 +5,7 @@ function fish_user_key_bindings
   bind \e\x7f backward-kill-bigword
   bind \ec    print_working_dir
   bind \em    serban_fish_prompt_minimal_toggle
+  bind \ej    'commandline --insert ·'
   bind \ey    'commandline --insert (date +%Y-%m-%d)'
   bind \ea    'fish_commandline_append " &| head -n 20"'
   bind \ez    'fish_commandline_append " &| tail -n 20"'
