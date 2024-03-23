a88da288e0cedf52308252ab7dc58da5d845cb2f
Serban Giuroiu <giuroiu@gmail.com>
2024-03-23 14:47:08 -0700

    fish: fn: Sort the output of fd
    fd only sorts its output when the search finishes in 100 milliseconds,
    but I want my notes sorted in lexicographical order always. See:
    
    • https://github.com/sharkdp/fd/commit/550e3b35723047065e76b461ceaf8a83619b6a4f
      ↳ Implement short-time buffering for sorted output
    • https://github.com/sharkdp/fd/issues/159#issuecomment-341225443
      ↳ deterministic order - by default or by option flag
    
    This is a follow-up to 9ba9811425ac048361e33456bbf22c4e341066bb.

--- fish/functions/fn.fish
+++ fish/functions/fn.fish
@@ -1,6 +1,6 @@
 function fn
   set --local theme (bat_theme)
-  fd --base-directory ~/txt --strip-cwd-prefix --type file \
+  fd --base-directory ~/txt --type file --strip-cwd-prefix | sort \
       | fzf --no-sort \
             --multi \
             --height 100% \
@@ -8,7 +8,7 @@ function fn
             --preview "bat --color always --theme '$theme' --plain ~/txt/{}" \
             --bind 'ctrl-g:change-preview-window(right,80|bottom,60%|hidden|)' \
             --bind 'ctrl-o:execute-silent(open -R ~/txt/{})' \
-            --bind 'ctrl-r:reload(fd --base-directory ~/txt --strip-cwd-prefix --type file)' \
+            --bind 'ctrl-r:reload(fd --base-directory ~/txt --type file --strip-cwd-prefix | sort)' \
             --bind 'double-click:execute-silent(mvim --remote-tab-silent ~/txt/{})' \
             --bind 'enter:execute-silent(mvim --remote-tab-silent ~/txt/{})' \
             --bind 'esc:cancel'
