87d11352117e50b4b51c7a9e43e4e24787800878
Serban Giuroiu <giuroiu@gmail.com>
2024-03-31 14:52:45 -0700

    py: Bump macOS Python to 3.12.2

--- bash/py.sh
+++ bash/py.sh
@@ -7,7 +7,7 @@ set -o pipefail
 if [[ -f /etc/debian_version ]]; then
   readonly PYTHON=/usr/bin/python3.11
 else
-  readonly PYTHON="${HOME}/env/pyenv/versions/3.12.1/bin/python3"
+  readonly PYTHON="${HOME}/env/pyenv/versions/3.12.2/bin/python3"
 fi
 
 exec -a python3 "${PYTHON}" "$@"
