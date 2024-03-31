5b469c0533f12501719b70e983cd60e3e0ff5ab7
Serban Giuroiu <giuroiu@gmail.com>
2024-03-31 14:31:13 -0700

    cv: Bump @ 2024-03-31

--- python/src/cv.py
+++ python/src/cv.py
@@ -24,22 +24,22 @@ class Package:
   mark: str = '✗'
 
 packages = [
-    Package('bat',       ['bat', '--version'],       r'bat (.+)',           '2023-12-31', '0.22.1',   '0.24.0'),
-    Package('delta',     ['delta', '--version'],     r'\033\[0mdelta (.+)', '2023-12-31', '0.16.5',   '0.16.5'),
-    Package('fd',        ['fd', '--version'],        r'fd(?:find)? (.+)',   '2023-12-31', '8.6.0',    '9.0.0'),
-    Package('fish',      ['fish', '--version'],      r'fish, version (.+)', '2023-12-31', '3.6.0',    '3.6.4'),
-    Package('fzf',       ['fzf', '--version'],       r'(.+) \(.*',          '2023-12-31', '0.38.0',   '0.44.1'),
-    Package('git',       ['git', '--version'],       r'git version (.+)',   '2023-12-31', '2.39.2',   '2.43.0'),
-    Package('less',      ['less', '--version'],      r'less (.+) \(.*',     '2023-12-31', '590',      '643'),
-    Package('lf',        ['lf', '--version'],        r'r?(.+)',             '2023-12-31', '28',       '31'),
-    Package('neovim',    ['nvim', '--version'],      r'NVIM v(.+)',         '2023-12-31', '0.9.5',    '0.9.5'),
-    Package('python',    ['python3', '--version'],   r'Python (.+)',        '2023-12-31', '3.11.2',   '3.12.1'),
-    Package('tmux',      ['tmux', '-V'],             r'tmux (.+)',          '2023-12-31', '3.3a',     '3.3a'),
-    Package('vim',       ['vim', '--version'],       VIM_REGEX,             '2023-12-31', '9.0.1378', '9.0.2150'),
-    Package('watchexec', ['watchexec', '--version'], r'watchexec (\S+)',    '2023-12-31', '1.24.2',   '1.24.2'),
-    Package('click',     None,                       None,                  '2023-12-31', '8.1.3',    '8.1.7'),
-    Package('packaging', None,                       None,                  '2023-12-31', '23.0',     '23.2'),
-    Package('rich',      None,                       None,                  '2023-12-31', '13.3.1',   '13.7.0'),
+    Package('bat',       ['bat', '--version'],       r'bat (.+)',           '2024-03-31', '0.22.1',   '0.24.0'),
+    Package('delta',     ['delta', '--version'],     r'delta (.+)',         '2024-03-31', '0.17.0',   '0.17.0'),
+    Package('fd',        ['fd', '--version'],        r'fd(?:find)? (.+)',   '2024-03-31', '8.6.0',    '9.0.0'),
+    Package('fish',      ['fish', '--version'],      r'fish, version (.+)', '2024-03-31', '3.6.0',    '3.7.1'),
+    Package('fzf',       ['fzf', '--version'],       r'(.+) \(.*',          '2024-03-31', '0.38.0',   '0.48.1'),
+    Package('git',       ['git', '--version'],       r'git version (.+)',   '2024-03-31', '2.39.2',   '2.44.0'),
+    Package('less',      ['less', '--version'],      r'less (.+) \(.*',     '2024-03-31', '590',      '643'),
+    Package('lf',        ['lf', '--version'],        r'r?(.+)',             '2024-03-31', '28',       '32'),
+    Package('neovim',    ['nvim', '--version'],      r'NVIM v(.+)',         '2024-03-31', '0.9.5',    '0.9.5'),
+    Package('python',    ['python3', '--version'],   r'Python (.+)',        '2024-03-31', '3.11.2',   '3.12.2'),
+    Package('tmux',      ['tmux', '-V'],             r'tmux (.+)',          '2024-03-31', '3.3a',     '3.4'),
+    Package('vim',       ['vim', '--version'],       VIM_REGEX,             '2024-03-31', '9.0.1378', '9.1.0250'),
+    Package('watchexec', ['watchexec', '--version'], r'watchexec (\S+)',    '2024-03-31', '1.25.1',   '1.25.1'),
+    Package('click',     None,                       None,                  '2024-03-31', '8.1.3',    '8.1.7'),
+    Package('packaging', None,                       None,                  '2024-03-31', '23.0',     '24.0'),
+    Package('rich',      None,                       None,                  '2024-03-31', '13.3.1',   '13.7.1'),
 ]
 
 for p in packages:
