f2140315d34da43279216d479e53d77defd25612
Serban Giuroiu <giuroiu@gmail.com>
2024-04-26 16:36:42 -0700

    hs: keys.lua: Split primary and secondary hotkeys

--- hammerspoon/serban/keys.lua
+++ hammerspoon/serban/keys.lua
@@ -2,15 +2,24 @@ local M = {}
 
 M.nexus = hs.host.names()[1] == 'nexus.local'
 
-M._modal = hs.hotkey.modal.new('⌘', 'e', 'Hotkey')
+M._modal_pri = hs.hotkey.modal.new('⌘', 'e', 'Hotkey')
+M._modal_sec = hs.hotkey.modal.new('⌘', 'd', 'Hotkey')
 
-function M._modal:entered()
-  hs.timer.doAfter(1.0, function() M._modal:exit() end)
+function M._modal_pri:entered()
+  hs.timer.doAfter(1.0, function() M._modal_pri:exit() end)
+end
+
+function M._modal_sec:entered()
+  hs.timer.doAfter(1.0, function() M._modal_sec:exit() end)
 end
 
 function M.bind(key, fn)
   hs.hotkey.bind('⌃⌥⇧⌘', key, fn)
-  M._modal:bind('', key, function() fn(); M._modal:exit() end)
+  M._modal_pri:bind('', key, function() fn(); M._modal_pri:exit() end)
+end
+
+function M.bind_sec(key, fn)
+  M._modal_sec:bind('', key, function() fn(); M._modal_sec:exit() end)
 end
 
 M.bind("'",     function() hs.grid.set(hs.window.focusedWindow(), {0, 0, 7, 9}) end) -- Maximize
@@ -44,28 +53,39 @@ M.bind('f8',    function() hs.osascript.applescript('tell application "System Ev
 M.bind('b',     function() activate('Firefox.app') end)
 M.bind('c',     function() activate('Google Chrome.app') end)
 M.bind('f',     function() activate('Finder.app') end)
-M.bind('g',     function() activate(M.nexus and 'Numbers.app' or 'Google Chat.app') end)
+M.bind('g',     function() activate('Numbers.app') end)
 M.bind('h',     function() activate('Weather.app') end)
 M.bind('i',     function() activate('Maps.app') end)
 M.bind('j',     function() activate('Sublime Merge.app') end)
-M.bind('k',     function() activate(M.nexus and 'Activity Monitor.app' or 'kitty.app') end)
+M.bind('k',     function() activate('kitty.app') end)
 M.bind('l',     function() activate('Preview.app') end)
 M.bind('m',     function() activate('Messages.app') end)
 M.bind('n',     function() activate('Notes.app') end)
 M.bind('o',     function() activate('OmniFocus.app') end)
-M.bind('p',     function() activate(M.nexus and 'Photos.app' or 'Photo Booth.app') end)
+M.bind('p',     function() activate('Photos.app') end)
 M.bind('q',     function() activate('Hammerspoon.app') end)
 M.bind('r',     function() activate('Calendar.app') end)
-M.bind('s',     function() activate(M.nexus and 'Safari.app' or 'Google Meet.app') end)
+M.bind('s',     function() activate('Safari.app') end)
 M.bind('t',     function() activate('iTerm.app') end)
-M.bind('u',     function() activate(M.nexus and 'UlyssesMac.app' or 'Cider V.app') end)
+M.bind('u',     function() activate('UlyssesMac.app') end)
 M.bind('v',     function() activate('MacVim.app') end)
 M.bind('x',     function() activate('IINA.app') end)
 M.bind('y',     function() activate('Dictionary.app') end)
-M.bind('z',     function() highlightMousePointer() end)
+M.bind('z',     function() activate('Zed.app') end)
 M.bind('4',     function() openFirefoxHomeTabs() end)
 M.bind('5',     function() openFirefoxChatTabs() end)
 M.bind('6',     function() openChromeHomeTabs() end)
 M.bind('7',     function() activate('TIDAL.app') end)
+
+M.bind_sec('a',     function() activate('Activity Monitor.app') end)
+M.bind_sec('c',     function() activate('Contacts.app') end)
+M.bind_sec('f',     function() activate('FindMy.app') end)
+M.bind_sec('o',     function() activate('Obsidian.app') end)
+M.bind_sec('p',     function() activate('Photo Booth.app') end)
+M.bind_sec('r',     function() activate('Reminders.app') end)
+M.bind_sec('v',     function() activate('Visual Studio Code.app') end)
+M.bind_sec('w',     function() activate('WezTerm.app') end)
+M.bind_sec('x',     function() activate('Xcode.app') end)
+M.bind_sec('z',     function() highlightMousePointer() end)
 
 return M
