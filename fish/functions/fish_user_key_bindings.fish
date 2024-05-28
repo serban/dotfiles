bbbc227f36ac9a76132ab486456aed423ccf2b02
Serban Giuroiu <giuroiu@gmail.com>
2024-05-28 12:53:44 -0700

    fish: fish_user_key_bindings: triangular bullet

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -5,6 +5,7 @@ function fish_user_key_bindings
   bind \e\x7f backward-kill-bigword
   bind \ec    print_working_dir
   bind \em    serban_fish_prompt_minimal_toggle
+  bind \ei    'commandline --insert ‣'
   bind \ej    'commandline --insert ·'
   bind \ey    'commandline --insert (date +%Y-%m-%d)'
   bind \ea    'fish_commandline_append " &| head -n 20"'
