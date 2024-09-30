cf6af5eff538d603b54b8719813a7bc5779458ec
Serban Giuroiu <giuroiu@gmail.com>
2024-09-30 14:46:37 -0700

    fish: fn: DRY up the search directory and commands

--- fish/functions/fn.fish
+++ fish/functions/fn.fish
@@ -1,20 +1,27 @@
 function fn
-  set --local theme (bat_theme)
   title fn
-  fd --base-directory ~/txt --type file --strip-cwd-prefix | sort \
+
+  set --local theme (bat_theme)
+  set --local root  '~/txt'
+  set --local bat   "bat --color always --theme '$theme' --plain $root/{}"
+  set --local fd    "fd --base-directory $root --type file --strip-cwd-prefix | sort"
+  set --local mvim  "mvim --remote-tab-silent $root/{}"
+  set --local open  "open -R $root/{}"
+
+  eval $fd \
       | fzf --no-sort \
             --multi \
             --height 100% \
             --preview-window right,60% \
-            --preview "bat --color always --theme '$theme' --plain ~/txt/{}" \
+            --preview $bat \
             --bind 'ctrl-g:change-preview-window(right,80|bottom,60%|hidden|)' \
-            --bind 'ctrl-o:execute-silent(open -R ~/txt/{})' \
-            --bind 'ctrl-r:reload(fd --base-directory ~/txt --type file --strip-cwd-prefix | sort)' \
-            --bind 'ctrl-s:reload(fd --base-directory ~/txt --type file --strip-cwd-prefix | sort -k 2)' \
-            --bind 'double-click:execute-silent(mvim --remote-tab-silent ~/txt/{})' \
-            --bind 'enter:execute-silent(mvim --remote-tab-silent ~/txt/{})' \
+            --bind "ctrl-o:execute-silent($open)" \
+            --bind "ctrl-r:reload($fd)" \
+            --bind "ctrl-s:reload($fd -k 2)" \
+            --bind "double-click:execute-silent($mvim)" \
+            --bind "enter:execute-silent($mvim)" \
             --bind 'esc:cancel'
 #           --print0 \
-#     | gsed --null-data 's|^|~/txt/|' \
+#     | gsed --null-data "s|^|$root/|" \
 #     | xargs -0 mvim --remote-tab-silent
 end
