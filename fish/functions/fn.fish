4c70d50386a3960044380a3c467469208f4c2433
Serban Giuroiu <giuroiu@gmail.com>
2024-10-10 12:43:58 -0700

    fish: fn: alt-{c,p}: ctrl-{x,v}: Copy file, path

--- fish/functions/fn.fish
+++ fish/functions/fn.fish
@@ -1,12 +1,27 @@
+# ╭────────────────────────────────────────────────────────────────────────────╮
+# │ Tips                                                                       │
+# ╰────────────────────────────────────────────────────────────────────────────╯
+# • fzf: Commands are executed with `$SHELL -c`, aka fish.
+# • fzf: The {} placeholder is already single-quoted by fzf.
+# • fzf: OSC 52 works with fzf execute() but not execute-silent().
+# • fish: Use fish command substitution `()` to remove a trailing newline.
+#   ↳ https://github.com/fish-shell/fish-shell/issues/3847#issuecomment-1321871256
+
 function fn
   title fn
 
   set --local theme (bat_theme)
+
   set --local root  '~/txt'
   set --local bat   "bat --color always --theme '$theme' --plain $root/{}"
   set --local fd    "fd --base-directory $root --type file --strip-cwd-prefix | sort"
   set --local mvim  "mvim --remote-tab-silent $root/{}"
   set --local open  "open -R $root/{}"
+
+  set --local clip_base  'echo -n (builtin path basename {}) | fish_clipboard_copy'
+  set --local clip_path  "echo -n '$root'/{} | fish_clipboard_copy"
+  set --local osc52_base 'osc52 (builtin path basename {})'
+  set --local osc52_path "osc52 '$root'/{}"
 
   eval $fd \
       | fzf --no-sort \
@@ -14,11 +29,15 @@ function fn
             --height 100% \
             --preview-window right,60% \
             --preview $bat \
+            --bind "alt-c:execute($osc52_base)" \
+            --bind "alt-p:execute($osc52_path)" \
             --bind 'ctrl-g:change-preview-window(right,80|bottom,60%|hidden|)' \
             --bind "ctrl-o:execute-silent($open)" \
             --bind "ctrl-r:reload($fd)" \
             --bind "ctrl-s:reload($fd --key 2)" \
             --bind "ctrl-t:reload($fd --reverse)" \
+            --bind "ctrl-v:execute-silent($clip_path)" \
+            --bind "ctrl-x:execute-silent($clip_base)" \
             --bind "double-click:execute-silent($mvim)" \
             --bind "enter:execute-silent($mvim)" \
             --bind 'esc:cancel'
