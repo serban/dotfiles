b2f1273f8d022b23b4fe1335c1372ce87ce4d0d2
Serban Giuroiu <giuroiu@gmail.com>
2024-10-11 11:48:15 -0700

    py: vimrc: Bump macOS Python to 3.13.0

--- bash/py.sh
+++ bash/py.sh
@@ -7,7 +7,7 @@ set -o pipefail
 if [[ -f /etc/debian_version ]]; then
   readonly PYTHON=/usr/bin/python3.11
 else
-  readonly PYTHON="${HOME}/env/pyenv/versions/3.13.0rc3/bin/python3"
+  readonly PYTHON="${HOME}/env/pyenv/versions/3.13.0/bin/python3"
 fi
 
 exec -a python3 "${PYTHON}" "$@"
