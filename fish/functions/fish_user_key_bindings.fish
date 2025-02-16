243a998b5d6ecfcbf843a254af16aa280ca68e7d
Serban Giuroiu <giuroiu@gmail.com>
2025-02-16 15:13:29 -0800

    fish: fish_user_key_bindings: Move by tokens
    As of 2025-02-16, the following token movement commands are available
    but not bound by default in the Fish 4 integration branch ｢1｣:
    
    ⁃ backward-token
    ⁃ forward-token
    ⁃ backward-kill-token
    ⁃ kill-token
    
    ■■■ Development History
    
    • https://github.com/fish-shell/fish-shell/pull/10738
      ↳ add token-cmds
    • https://github.com/fish-shell/fish-shell/pull/10766
      ↳ Default bindings for token movement commands
    • https://github.com/fish-shell/fish-shell/issues/10926
      ↳ behaviour of option+backspace differs between v3 and v4
    
    • https://github.com/fish-shell/fish-shell/commit/a9cee9e75583e43f5e443d96b83a00e81dda4dea
      ↳ 2024-10-05: Commands to move by entire tokens
    • https://github.com/fish-shell/fish-shell/commit/6af96a81a8c6ff5e632d4dda7448f5f01d7a5d35
      ↳ 2024-10-13: Default bindings for token movement commands
    • https://github.com/fish-shell/fish-shell/commit/378f452eaa0abbe1a6e3d41963adfcee11803a09
      ↳ 2025-02-05: Revert token movement bindings
    
    ｢1｣ https://github.com/fish-shell/fish-shell/tree/Integration_4.0.0

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,9 +1,16 @@
 function fish_user_key_bindings
   bind \cc                cancel-commandline
+if test (string split . --fields 1 $FISH_VERSION) -ge 4
+  bind alt-left           prevd-or-backward-token   # __fish_shared_key_bindings
+  bind alt-right          nextd-or-forward-token    # __fish_shared_key_bindings
+  bind alt-backspace      backward-kill-token       #  fish_default_key_bindings
+  bind alt-delete         kill-token                #  fish_default_key_bindings
+else
   bind \ef                forward-bigword
   bind \eb                backward-bigword
   bind \ed                kill-bigword
   bind \e\x7f             backward-kill-bigword
+end
   bind \ec                print_working_dir
   bind \ev                cycle_ripgrep_wrapper
   bind \em                serban_fish_prompt_minimal_toggle
