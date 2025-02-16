76e89094a16e3e3a8c092a27acd9c3490211e246
Serban Giuroiu <giuroiu@gmail.com>
2025-02-16 12:08:48 -0800

    fish: fish_user_key_bindings: no-op: Indent

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,15 +1,15 @@
 function fish_user_key_bindings
-  bind \cc    cancel-commandline
-  bind \ef    forward-bigword
-  bind \eb    backward-bigword
-  bind \ed    kill-bigword
-  bind \e\x7f backward-kill-bigword
-  bind \ec    print_working_dir
-  bind \em    serban_fish_prompt_minimal_toggle
-  bind \eg    serban_fish_prompt_version_toggle
-  bind \ei    'commandline --insert ‣'
-  bind \ej    'commandline --insert ·'
-  bind \ey    'commandline --insert (date +%Y-%m-%d)'
-  bind \ea    'fish_commandline_append " &| head -n 20"'
-  bind \ez    'fish_commandline_append " &| tail -n 20"'
+  bind \cc                cancel-commandline
+  bind \ef                forward-bigword
+  bind \eb                backward-bigword
+  bind \ed                kill-bigword
+  bind \e\x7f             backward-kill-bigword
+  bind \ec                print_working_dir
+  bind \em                serban_fish_prompt_minimal_toggle
+  bind \eg                serban_fish_prompt_version_toggle
+  bind \ei                'commandline --insert ‣'
+  bind \ej                'commandline --insert ·'
+  bind \ey                'commandline --insert (date +%Y-%m-%d)'
+  bind \ea                'fish_commandline_append " &| head -n 20"'
+  bind \ez                'fish_commandline_append " &| tail -n 20"'
 end
