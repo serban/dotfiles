c1ff6d7a857415c5524e84d2dded123472482987
Serban Giuroiu <giuroiu@gmail.com>
2025-09-03 18:56:52 -0700

    make-home-dirs.sh: Make dirs with 700 permissions
    Make directory and symlink creation idempotent.

--- setup/make-home-dirs.sh
+++ setup/make-home-dirs.sh
@@ -4,9 +4,59 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-mkdir -vp ~/bin ~/env ~/frk ~/git ~/log ~/oss ~/pkg ~/pre ~/run ~/ses ~/src ~/txt ~/uvd ~/wks
+readonly -a kFoldersCommon=(
+
+  bin
+  env
+  frk
+  git
+  log
+  oss
+  pkg
+  pre
+  run
+  ses
+  src
+  txt
+  uvd
+  wks
+
+)
+
+readonly -a kFoldersDarwin=(
+
+  Data
+  Deletable
+  Screenshots
+
+)
+
+declare -A kSymlinksDarwin=(
+
+# [Data]='Library/Mobile Documents/com~apple~CloudDocs/Data'
+  [Workspace]=wks
+
+)
+
+function makedir {
+  mkdir -vpm 700 "${1}"
+  chmod -v   700 "${1}"
+}
+
+cd
+umask u=rwx,g=,o=  # umask 077
+
+for folder in "${kFoldersCommon[@]}"; do
+  makedir "${folder}"
+done
 
 if [[ "$(uname -s)" == Darwin ]]; then
-  mkdir -vp ~/Data ~/Deletable ~/Screenshots
-  ln -vns wks ~/Workspace
+  for folder in "${kFoldersDarwin[@]}"; do
+    makedir "${folder}"
+  done
+
+  for link in "${!kSymlinksDarwin[@]}"; do
+#   echo "${link}" → "${kSymlinksDarwin[${link}]}"
+    ln -vsnf "${kSymlinksDarwin[${link}]}" "${link}"
+  done
 fi
