b851f885d1ac1f6aae76c0a7ea32810618c900ee
Serban Giuroiu <giuroiu@gmail.com>
2025-11-05 08:20:07 -0800

    nvim: Map <F1> to <Esc>

--- nvim/lua/serban/core/keys.lua
+++ nvim/lua/serban/core/keys.lua
@@ -5,6 +5,11 @@ vim.keymap.set('n', 'Q', '<Nop>')
 
 vim.keymap.set('n', 'gQ', '<Nop>')
 
+vim.keymap.set('c', '<F1>', '<Esc>')
+vim.keymap.set('n', '<F1>', '<Esc>')
+vim.keymap.set('i', '<F1>', '<Esc>')
+vim.keymap.set('v', '<F1>', '<Esc>')
+
 vim.keymap.set('n', '<C-a>', ':botright vsplit <CR>')
 vim.keymap.set('n', '<C-g>', ':split <CR>')
 
@@ -20,8 +25,8 @@ vim.keymap.set('n', '<C-k>', ':wincmd k <CR>')
 vim.keymap.set('n', '<C-l>', ':wincmd l <CR>')
 
 vim.keymap.set('n', '<C-s>', ':update <CR>')
-vim.keymap.set('i', '<C-s>', '<ESC>:update <CR>')
-vim.keymap.set('v', '<C-s>', '<ESC>:update <CR>')
+vim.keymap.set('i', '<C-s>', '<Esc>:update <CR>')
+vim.keymap.set('v', '<C-s>', '<Esc>:update <CR>')
 vim.keymap.set('n', '<C-q>', vim.cmd.quitall)
 
 vim.keymap.set('n', '<M-n>', ':bnext <CR>')
