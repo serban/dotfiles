b0bd37b0dae558fde35bacee2eee3893c9d0faee
Serban Giuroiu <giuroiu@gmail.com>
2026-04-27 11:50:01 -0700

    cv: Check version of just

--- python/src/cv.py
+++ python/src/cv.py
@@ -35,6 +35,7 @@ kPackages = [
   Package('fish',      ['fish', '--version'],      r'fish, version (.+)', '2025-08-11', '4.0.2',    '4.0.2',    '4.0.2'),
   Package('fzf',       ['fzf', '--version'],       r'(.+) \(.*',          '2025-08-11', '0.60',     '0.60',     '0.65.1'),
   Package('git',       ['git', '--version'],       r'git version (.+)',   '2025-08-11', '2.47.2',   '2.47.2',   '2.50.1'),
+  Package('just',      ['just', '--version'],      r'just (.+)',          '2026-04-21', '1.40.0',   '1.40.0',   '1.50.0'),
   Package('less',      ['less', '--version'],      r'less (.+) \(.*',     '2025-08-11', '668',      '668',      '679'),
   Package('lf',        ['lf', '--version'],        r'r?(.+)',             '2025-08-11', '34',       '34',       '36'),
   Package('neovim',    ['nvim', '--version'],      r'NVIM v(.+)',         '2025-08-11', '0.10.4',   '0.10.4',   '0.11.3'),
